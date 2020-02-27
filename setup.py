9c32505e94f1f781e37e4af4b6157697685cc80d
Abiezer Sifotnes <ajsifontes@urbos.io>
2020-02-27 12:06:02 -0300

    update dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 setup(
   name = 'fe_openti',
   packages = find_packages(), # this must be the same as the name above
-  version = '0.0.1',
+  version = '0.0.3',
   description = 'this module add changes to fe of openti',
   author = 'Abiezer Sifontes',
   author_email = 'ajsifontes@urbos.io',
   url = 'https://github.com/UrbosSmartCity/fe_openti', # use the URL to the github repo
-  download_url = 'https://github.com/RDCH106/urbossmartcity/fe_openti/archive/0.0.1.tar.gz',
+  download_url = 'https://github.com/RDCH106/urbossmartcity/fe_openti/archive/0.0.3.tar.gz',
   classifiers=['Programming Language :: Python',  # Clasificadores de compatibilidad con versiones de Python para tu paqeute
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.3',
